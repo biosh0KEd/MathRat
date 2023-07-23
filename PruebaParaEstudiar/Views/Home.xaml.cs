6aa738e4f2c049f02c30a2f2a0dc11ff9039ec34
Efraín Girón Romero <efrainbond@hotmail.com>
2023-07-23 13:19:09 -0600

    Project initialized, round main window added

---
+++ PruebaParaEstudiar/Views/Home.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PruebaParaEstudiar.Views;
+
+public partial class Home : UserControl
+{
+    public Home()
+    {
+        InitializeComponent();
+    }
+}
