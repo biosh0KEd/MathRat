caf41610bbb77b3e9e6c37fb3a70781c0eb34c87
Efrain Giron <efrainbond@hotmail.com>
2023-10-11 23:42:03 -0600

    Cambio de la animación de hover de los botones de maximizar y minimizar.

--- MathRat/ViewModels/NavigationViewModel.cs
+++ MathRat/ViewModels/NavigationViewModel.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using MathRat.Models.General;
 using MathRat.Resources.Utilities;
